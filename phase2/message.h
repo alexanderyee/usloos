9826eb4a273148936277d10717dd2717d38d7024
i 3 booty <yeealexi@gmail.com>
2017-09-24 14:19:43 -0700

    phase2 init

---
+++ phase2/message.h
@@ -0,0 +1,35 @@
+
+#define DEBUG2 1
+
+typedef struct mailSlot *slotPtr;
+typedef struct mailbox   mailbox;
+typedef struct mboxProc *mboxProcPtr;
+
+struct mailbox {
+    int       mboxID;
+    int       isUsed;
+    int       numSlots;	
+    slotPtr   childSlots[MAXSLOTS];
+    // other items as needed...
+	
+};
+
+struct mailSlot {
+    int       mboxID;
+    int       status;
+    void      *data;
+    // other items as needed...
+};
+
+struct psrBits {
+    unsigned int curMode:1;
+    unsigned int curIntEnable:1;
+    unsigned int prevMode:1;
+    unsigned int prevIntEnable:1;
+    unsigned int unused:28;
+};
+
+union psrValues {
+    struct psrBits bits;
+    unsigned int integerPart;
+};
