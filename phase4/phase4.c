9127d6dc928802bdc7e0b83ec2ad46992a08d871
alexanderyee <yeealexi@gmail.com>
2017-11-02 19:36:37 -0700

    parameter check numero DOS

--- phase4/phase4.c
+++ phase4/phase4.c
@@ -240,7 +240,24 @@ int diskReadReal(USLOSS_Sysargs * args)
 
     diskSizeReal(unit, sectorSize, numSectors, numTracks);
     // check if first and sectors are > 0 and < numsectors; track > 0 and < numTracks
-    diskEnqueue(dbuff, unit, track, first, sectors);
+  	if(first < 0 || first >= numSectors){
+		USLOSS_Console("diskSizeReal() first is invalid\n");
+        USLOSS_Halt(1);
+        return -1;	
+	}
+	
+	if(sectors < 0 || sectors >= numSectors){
+        USLOSS_Console("diskSizeReal() sectors is invalid\n");
+        USLOSS_Halt(1);
+        return -1;
+    }
+
+	if(track < 0 || track >= numTracks){
+        USLOSS_Console("diskSizeReal() track is invalid\n");
+        USLOSS_Halt(1);
+        return -1;
+    }
+	diskEnqueue(dbuff, unit, track, first, sectors);
 }
 
 
