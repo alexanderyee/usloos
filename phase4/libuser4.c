662c42f970d8eed8475419f01eaf92f21f39f7fa
Katie Pan <katie@efferential.com>
2017-11-06 14:19:07 -0700

    termWrite & termRead finished on libuser4

--- phase4/libuser4.c
+++ phase4/libuser4.c
@@ -119,6 +119,9 @@ int TermWrite(char *buff, int bsize, int unit_id, int *nwrite)
     sysArg.arg2 = (void *) (long) bsize;
     sysArg.arg3 = (void *) (long) unit_id;
     sysArg.arg4 = nwrite;
+    USLOSS_Syscall(&sysArg);
+    nwrite = (long) sysArg.arg2;
+	return (long) sysArg.arg4;
 
 	return 0;
 
